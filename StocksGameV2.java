5eef3aeafe2cef29d1262db1f53c05638fc9fa91
Aymaan <34725789+ashaikh23@users.noreply.github.com>
2020-05-19 12:32:08 -0400

    Readjusted based on ION specifications
    /var/folders/v5/6c4xx0pd58g55zs6kfkjc9qm0000gq/T/com.apple.iChat/Messages/Transfers/Screen Shot 2020-05-18 at 4.04.54 PM.jpeg

--- StocksGameV2.java
+++ StocksGameV2.java
@@ -1,8 +1,9 @@
-import java.util.Random;
 public class StocksGameV2 {
-
+	
 	public static void main(String[] args) {
-		Random rand = new Random();
+		
+		final String name = "Shaikh's Stock Generator!";
+		final String fName = "Aymaan ";
 		
 		//-----------------------------
 		int cheepStockMinValue = 100;
@@ -25,11 +26,15 @@ public class StocksGameV2 {
 				
 		//------------------------------
 		int cheepStock1 = (int)(Math.random() * (cheepStockMaxValue-cheepStockMinValue+1)+cheepStockMinValue);
+		int cheepStock2 = (int)(Math.random() * (cheepStockMaxValue-cheepStockMinValue+1)+cheepStockMinValue);
 		
 		int mediumStock1 = (int)(Math.random() *  (mediumStockMaxValue-mediumStockMinValue+1)+mediumStockMinValue);
+		int mediumStock2 = (int)(Math.random() *  (mediumStockMaxValue-mediumStockMinValue+1)+mediumStockMinValue);
 		
 		int adequateStock1 = (int)(Math.random() * (adequateStockMaxValue-adequateStockMinValue+1)+adequateStockMinValue);
 		int adequateStock2 = (int)(Math.random() * (adequateStockMaxValue-adequateStockMinValue+1)+adequateStockMinValue);
+		int adequateStock3 = (int)(Math.random() * (adequateStockMaxValue-adequateStockMinValue+1)+adequateStockMinValue);
+		//BETA int adequateStock4 = (int)(Math.random() * (adequateStockMaxValue-adequateStockMinValue+1)+adequateStockMinValue);
 		
 		int highStock1 = (int)(Math.random() * (highStockMaxValue-highStockMinValue+1) + highStockMinValue);
 		
@@ -38,46 +43,58 @@ public class StocksGameV2 {
 		int flexableStock1 = (int)(Math.random() * (flexableStockMaxValue-flexableStockMinValue+1) + flexableStockMinValue);
 		//------------------------------
 		
-		/*
-		 * AIMON Stock = flexableStock1, Direxion Gold Miners = inflatedStock1, highStock1 = highStock1, adequateStock1 = adequateStock1, mediumStock1 = mediumStock1, cheepStock1 = cheepStock1
-		 *BETA Feature
-		 */
-		
+		//------------------------------
 		int roundedCheepStock1 = ((cheepStock1 + 99) / 100 ) * 100;
+		int roundedCheepStock2 = ((cheepStock2 + 99) / 100 ) * 100;
 		
 		int roundedMediumStock1 = ((mediumStock1 + 99) / 100 ) * 100;
+		int roundedMediumStock2 = ((mediumStock2 + 99) / 100 ) * 100;
 		
 		int roundedAdequateStock1 = ((adequateStock1 + 99) / 100 ) * 100;
-		
 		int roundedAdequateStock2 = ((adequateStock2 + 99) / 100 ) * 100;
+		int roundedAdequateStock3 = ((adequateStock3 + 99) / 100 ) * 100;
+		//BETA int roundedAdequateStock4 = ((adequateStock4 + 99) / 100 ) * 100;
 		
 		int roundedHighStock1 = ((highStock1 + 99) / 100 ) * 100;
 		
 		int roundedInflatedStock1 = ((inflatedStock1 + 99) / 100 ) * 100;
 		
 		int roundedFlexableStock1 = ((flexableStock1 + 99) / 100 ) * 100;
+		//------------------------------
 		
 		
-		System.out.println("Aymaan's Shaikh Stock Generator!");
-		
-		System.out.println("----------------------------------");
-		System.out.println("TEAMVIEWER: $" + roundedCheepStock1);
-		System.out.println("Panasonic: $" + roundedMediumStock1);
-		System.out.println("Dell Inc: $" + roundedAdequateStock1);
-		System.out.println("Chipotle: $" + roundedAdequateStock2);
-		System.out.println("Apple : $" + roundedHighStock1);
-		System.out.println("Amazon: $" + roundedInflatedStock1);
-		System.out.println("AIMON: $" + roundedFlexableStock1);
+		System.out.println(fName + name);
 		System.out.println("----------------------------------");
 		
+		System.out.println("Direxion Gold Miners: $" + roundedFlexableStock1);
+		System.out.println("Tesla Inc.: $" + roundedInflatedStock1);
+		System.out.println("Alphabet Inc. : $" + roundedHighStock1);
+		System.out.println("Apple Inc: $" + roundedAdequateStock1);
+		System.out.println("Microsoft: $" + roundedAdequateStock2);
+		System.out.println("Amazon: $" + roundedAdequateStock3);
+		//BETA System.out.println("Dell Inc. $" + roundedAdequateStock4);
+		System.out.println("Walmart: $" + roundedMediumStock1);
+		System.out.println("Zynga Inc. :" + roundedMediumStock2);
+		System.out.println("Yahoo: $" + roundedCheepStock1);
+		System.out.println("Coca Cola Co. : $" + roundedCheepStock2);
+		
 		System.out.println("----------------------------------");
-		System.out.println(roundedCheepStock1);
-		System.out.println(roundedMediumStock1);
+		
+		System.out.println("For Table");
+		System.out.println("\n");
+		
+		System.out.println( roundedFlexableStock1);
+		System.out.println(roundedInflatedStock1);
+		System.out.println(roundedHighStock1);
 		System.out.println(roundedAdequateStock1);
 		System.out.println(roundedAdequateStock2);
-		System.out.println(roundedHighStock1);
-		System.out.println(roundedInflatedStock1);
-		System.out.println(roundedFlexableStock1);
-					
+		System.out.println(roundedAdequateStock3);
+		//BETA System.out.println(roundedAdequateStock4);
+		System.out.println(roundedMediumStock1);
+		System.out.println(roundedMediumStock2);
+		System.out.println(roundedCheepStock1);
+		System.out.println(roundedCheepStock2);
+									
 	}
+	
 }
